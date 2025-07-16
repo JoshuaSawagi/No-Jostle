27b8886bdb70a550f56ff37d0d106dc6bdcf97b0
Sawagi <132923266+JoshuaSawagi@users.noreply.github.com>
2025-07-16 12:10:38 -0500

    Add files via upload

---
+++ src/fighters/ike/frame.rs
@@ -0,0 +1,22 @@
+use smashline::*;
+use smash::hash40;
+use smash_script::*;
+use smash::lua2cpp::*;
+use smash::phx::Hash40;
+use smash::app::lua_bind::*;
+use smash::lib::lua_const::*;
+use crate::common::global_fighter_frame;
+
+unsafe extern "C" fn global_on_main(fighter: &mut L2CFighterCommon) {
+    unsafe {
+
+        global_fighter_frame(fighter);
+    }
+}
+
+
+pub fn install() {
+	Agent::new("ike")
+	.on_line(Main, global_on_main)
+	.install();
+}
